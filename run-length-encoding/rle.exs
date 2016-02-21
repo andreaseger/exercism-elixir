474b5f1491928e8ef30207bc1f0d7f0a3c1fb14e
Andreas Eger <dev@eger-andreas.de>
2016-02-21 13:29:33 +0100

    improve rle encode using regex

--- run-length-encoding/rle.exs
+++ run-length-encoding/rle.exs
@@ -6,27 +6,15 @@ defmodule RunLengthEncoder do
   It should also be able to reconstruct the data into its original form. 
   "1H1O1R1S1E" => "HORSE" 
   """
-  @spec encode(string) :: String.t
+  @spec encode(String.t) :: String.t
   def encode(""), do: ""
   def encode(string) do
-    string
-      |> String.codepoints
-      |> _encode
-      |> Enum.reverse
-      |> Enum.map_join(fn({char, count}) -> "#{count}#{char}" end)
-  end
-  defp _encode(list), do: _encode(list, [])
-  defp _encode([], acc), do: acc
-  defp _encode([h|tail], []), do: _encode(tail, [{h, 1}])
-  defp _encode([h|tail], acc=[{last_char, last_char_count}|t_acc]) do
-    if last_char == h do
-      _encode(tail, [{last_char, last_char_count + 1}|t_acc])
-    else
-      _encode(tail, [{h, 1}| acc])
-    end
+    Regex.scan(~r{(.)\1*}, string, capture: :first)
+      |> List.flatten
+      |> Enum.map_join(fn run -> "#{String.length(run)}#{String.first(run)}" end)
   end
 
-  @spec decode(string) :: String.t
+  @spec decode(String.t) :: String.t
   def decode(""), do: ""
   def decode(string) do
     Regex.scan(~r{(\d+)(\w)}, string)
