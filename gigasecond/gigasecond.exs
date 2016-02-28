3de97143d393fc71fc0a650fe2dcb378318dc6aa
Andreas Eger <dev@eger-andreas.de>
2016-02-28 22:07:27 +0100

    improve gigasecond

--- gigasecond/gigasecond.exs
+++ gigasecond/gigasecond.exs
@@ -5,7 +5,10 @@ defmodule Gigasecond do
 	@spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) :: :calendar.datetime
 
 	def from(datetime) do
-    seconds = :calendar.datetime_to_gregorian_seconds(datetime)
-    1.0e9 + seconds |> round |> :calendar.gregorian_seconds_to_datetime
+    datetime
+      |> :calendar.datetime_to_gregorian_seconds
+      |> +(1.0e9)
+      |> round
+      |> :calendar.gregorian_seconds_to_datetime
 	end
 end
