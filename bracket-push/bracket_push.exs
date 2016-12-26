c475065436a36efecc5e4d258448ffb0b5c28b2b
Andreas Eger <dev@eger-andreas.de>
2016-12-26 21:51:06 +0100

    solve bracket-push

--- bracket-push/bracket_push.exs
+++ bracket-push/bracket_push.exs
@@ -4,6 +4,16 @@ defmodule BracketPush do
   """
   @spec check_brackets(String.t) :: boolean
   def check_brackets(str) do
-
+    String.graphemes(str)
+      |> balanced?([])
   end
+
+  defp balanced?([],[]), do: true
+  defp balanced?([],_), do: false
+  defp balanced?([c|rest], stack) when c in ["{", "[", "("], do: balanced?(rest, [c|stack])
+  defp balanced?(["]"|rest], ["["|stack]), do: balanced?(rest, stack)
+  defp balanced?([")"|rest], ["("|stack]), do: balanced?(rest, stack)
+  defp balanced?(["}"|rest], ["{"|stack]), do: balanced?(rest, stack)
+  defp balanced?([c|_rest], _stack) when c in ["}","]",")"], do: false
+  defp balanced?([_|rest], stack), do: balanced?(rest, stack)
 end
